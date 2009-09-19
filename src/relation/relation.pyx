6c0322b301a93336288b1ed0d62aca3d190eccef
Alex Lee <lee@ccs.neu.edu>
2009-09-19 10:42:59 -0400

    Initial port of edu.neu.ccs.evergreen.*

---
+++ src/relation/relation.pyx
@@ -0,0 +1,352 @@
+
+ctypedef unsigned long int uint32_t
+
+cdef int SOURCE
+SOURCE = 0
+cdef int TARGET
+TARGET = 1
+
+MAGIC_NUMBERS = ( 0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF, 0x0000FFFF )
+
+
+def get_magic_number(int rank, int var_p, int value):
+    cdef uint32_t mask
+    cdef uint32_t col
+    mask = get_mask(rank)
+    col = ((MAGIC_NUMBERS[var_p]) & mask)
+    if value == 1:
+        return mask ^ col
+    return col
+
+
+def get_mask(int rank):
+    return (
+        0x1,
+        0x3,
+        0xF,
+        0xFF,
+        0xFFFF,
+        0xFFFFFFFF,
+    )[rank]
+
+
+def is_irrelevant(uint32_t rn, int rank, int var_p):
+    # Note that the maximum value of variable position is less than rank by one
+    # Therefore, right shift by (1<<var_p) won't reach 32 therefore, it'll always execute
+    cdef uint32_t m
+    m = get_magic_number(rank, var_p, 1)
+    # Must use >>> (unsigned shift right) coz relations of rank 5 might be -ve
+    return ((rn & m) >> (1 << var_p)) == (rn & (~m))
+    
+# Counts the number of relevantVariables in the given relation
+# @param relationNumber the relation number whose number of relevant variables is to be counted
+# @param rank rank of the given relation
+# @return The number of relevant variables in the given relation
+def num_relevant_variables(uint32_t rn, int rank):
+    # checkRank(rank);
+    # checkRelationNumber(relationNumber, rank);
+   
+    cdef int rel_vars
+    cdef int i
+
+    rel_vars = rank
+
+    for 0 <= i < rank:
+        if is_irrelevant(rn, rank, var_p):
+            rel_vars -= 1
+        i += 1
+    return rel_vars
+
+
+# Checks if the given relation forces the given variablePosition
+# @param relationNumber
+# @param rank rank of the given relation
+# @param variablePosition positon of the varible checked for being forced
+# @return 0 if the given relation forces the given variable to 0
+#         1 if the given relation forces the given variable to 1
+#         -1 given relation doesn't forces the given variable
+def is_forced(uint32_t rn, int rank, int var_p):
+    # checkRank(rank);
+    # checkRelationNumber(rn, rank);
+    # checkVariablePosition(var_p, rank);
+    
+    cdef uint32_t m
+    cdef uint32_t rm
+
+    if is_irrelevant(rn, rank, var_p):
+        return -1
+    else:
+        m = get_magic_number(rank, var_p, 1)
+        rm = rn & m;
+        if rm == 0:
+            return 0
+        elif rm == rn:
+            return 1
+        else:
+            return -1
+
+
+# starting at the given startPosition, get the position of the first variable forced by the given relation.
+# @param relationNumber relation number
+# @param rank rank of the given relation number
+# @param startPosition
+# @return -1 if nothing is forced
+#         the position of the first forced variable
+def first_forced_variable(uint32_t rn, int rank, int start_p):
+    # checkRank(rank);
+    # checkRelationNumber(relationNumber, rank);
+    # checkVariablePosition(startPosition, rank);
+   
+    for var_p in range(start_p, rank):
+        if is_forced(rn, rank, var_p) != -1:
+            return var_p
+    
+# NMaps one of the variables in a relation i.e. replaces it by it's complement
+# for example: nMapping x in Or(x,y,z) results in: or(!x,y,z)
+# @param relationNumber 
+# @param rank rank of the given relation
+# @param variablePosition the variable to be nmapped
+# @return The number of the given relation with the specified variable nmapped
+def n_map(uint32_t rn, int rank, int var_p):
+    # checkRank(rank);
+    # checkRelationNumber(rn, rank);
+    # checkVariablePosition(var_p, rank);
+
+    cdef uint32_t m0
+    cdef uint32_t m1
+    cdef uint32_t s
+
+    m0 = get_magic_number(rank, var_p, 0)
+    m1 = get_magic_number(rank, var_p, 1)
+    s = (1 << var_p)
+    return ((rn & m0) << s) | ((rn & m1) >>s)
+
+
+# Reduces a relation by assigning a value to one of its variables
+# @param rn
+# @param rank
+# @param var_p
+# @param value
+# @return
+def reduce(uint32_t rn, int rank, int var_p, int value):
+    # checkRank(rank);
+    # checkRelationNumber(rn, rank);
+    # checkVariablePosition(var_p, rank);
+    # checkValue(value);
+
+    cdef uint32_t m
+    cdef uint32_t rm
+
+    m = get_magic_number(rank, var_p, value);
+    rm = (rn & m)
+
+    if value == 0:
+        return rm | (rm << (1 << var_p))
+    else:
+        return rm | (rm >> (1 << var_p))
+
+
+
+# Swaps two variables in a relation. When variables are swapped, The truth table order gets scrambled
+# rows of the truth table needs to be reordered to restore the correct truth table order.
+# Here are two exmples showing how the swap method works for two relations:
+# 1in3(x,y,z), x implies z. We are swapping variables at positions 0,2 i.e: x ,z
+# 
+#      original relations            scrambled truth table      restored truth table ordering  
+# Row# x y z 1in3(x,y,z)  x=>z  ||  z y x 1in3(z,y,x) x=>z  ||  z y x 1in3(z,y,x) x=>z old_Row#
+# ------------------------------||--------------------------||---------------------------------
+# 0    0 0 0   0           1    ||  0 0 0   0          1    ||  0 0 0    0         1      0
+# 1    0 0 1   1           1    ||  1 0 0   1          1    ||  0 0 1    1         0      4
+# 2    0 1 0   1           1    ||  0 1 0   1          1    ||  0 1 0    1         1      2
+# 3    0 1 1   0           1    ||  1 1 0   0          1    ||  0 1 1    0         0      6
+# 4    1 0 0   1           0    ||  0 0 1   1          0    ||  1 0 0    1         1      1
+# 5    1 0 1   0           1    ||  1 0 1   0          1    ||  1 0 1    0         1      5
+# 6    1 1 0   0           0    ||  0 1 1   0          0    ||  1 1 0    0         1      3
+# 7    1 1 1   0           1    ||  1 1 1   0          1    ||  1 1 1    0         1      7
+# 
+# @param rn
+# @param rank
+# @param var_p1
+# @param var_p2
+# @return
+def swap(uint32_t rn, int rank, int var_p1, int var_p2):
+    # checkRank(rank);
+    # checkRelationNumber(rn, rank);
+    # checkVariablePosition(var_p1, rank);
+    # checkVariablePosition(var_p1, rank);
+    
+    cdef int tmp
+    cdef uint32_t d10
+    cdef uint32_t d11
+    cdef uint32_t d20
+    cdef uint32_t d21
+    cdef uint32_t same_filter
+    cdef uint32_t up_filter
+    cdef uint32_t down_filter
+    cdef uint32_t shift_amt
+    cdef uint32_t s
+    cdef uint32_t u
+    cdef uint32_t d
+
+    # Swapping the variable with itself
+    if var_p1 == var_p2:
+        return rn;
+    # swap Dim1,Dim2; 
+    # Can't we tell java to automatically do the swap by just stating the condition that dim1>dim2
+    if var_p1 > var_p2:
+        tmp = var_p1
+        var_p1 = var_p2
+        var_p2 = tmp
+    
+    d10 = get_magic_number(rank, var_p1, 0)
+    d11 = get_magic_number(rank, var_p1, 1)
+    d20 = get_magic_number(rank, var_p2, 0)
+    d21 = get_magic_number(rank, var_p2, 1) 
+    
+    # 1in3(x,y,z), swap x,z
+    # x=>z
+    #      original relations            scrambled truth table      restored truth table ordering  
+    # Row# x y z 1in3(x,y,z)  x=>z  ||  z y x 1in3(z,y,x) x=>z  ||  z y x 1in3(z,y,x) x=>z old_Row#
+    # ------------------------------||--------------------------||---------------------------------
+    # 0    0 0 0   0           1    ||  0 0 0   0          1    ||  0 0 0    0         1      0
+    # 1    0 0 1   1           1    ||  1 0 0   1          1    ||  0 0 1    1         0      4
+    # 2    0 1 0   1           1    ||  0 1 0   1          1    ||  0 1 0    1         1      2
+    # 3    0 1 1   0           1    ||  1 1 0   0          1    ||  0 1 1    0         0      6
+    # 4    1 0 0   1           0    ||  0 0 1   1          0    ||  1 0 0    1         1      1
+    # 5    1 0 1   0           1    ||  1 0 1   0          1    ||  1 0 1    0         1      5
+    # 6    1 1 0   0           0    ||  0 1 1   0          0    ||  1 1 0    0         1      3
+    # 7    1 1 1   0           1    ||  1 1 1   0          1    ||  1 1 1    0         1      7
+    
+    # Rows with either 0 or 1 in both columns stay the same
+    # e.g. row 0, row 7, other rows depending on the two swapped columns
+    # same_filter selects these rows based on the columns
+    same_filter = d11 & d21 | d10 & d20
+    # Assuming that column1 is to the right of column2 which is valid by the swapping we do at the begining of this method
+    # Rows where column1 is 0 and column2 is 1 must be moved up because after doing the swap we'll have a 0 in column 2 and 1 in column 1
+    # simply because the 0 in column2 means that the row number of column2 becomes smaller than the row number of column1. to restore the proper numbering
+    # we have to swap the two rows
+    up_filter = d21 & d10
+    # Select rows to be moved down. the reasoning is similar to up_filter
+    dn_filter = d20 & d11
+    # shift_amount is the difference between the row number in the before we do the swap and the row number after we do the swap
+    # for example if we are swapping the variable at position 2 with the variable at position 0 then the rows to be swapped are of the form        
+    #  bbbbb1b0 and bbbbb0b1 where b stands for an arbitrary bits that stays the same.
+    #  Noting that: bbbbb1b0 - bbbbb0b1 is the same as: (bbbbb1b0-bbbbb0b0) - (bbbbb0b1-bbbbb0b0)
+    #  by subtracting bbbbb0b0 from both numbers we get
+    #  00000100 and 00000001
+    #  Therefore we need to shift both ways by 2^var_p2 - 2^var_p1
+    
+    shift_amt = (1 << var_p2) - (1 << var_p1)
+    
+    #  rows to stay at their locations
+    s = rn & same_filter
+    # rows to go up
+    u = rn & up_filter
+    # rows to go down
+    d = rn & dn_filter
+    
+    # move the rows and combine the three components
+    return (s | (d << shift_amt) | (u >> shift_amt))
+
+
+
+# permute the variables in the given relationNumber according to the given permutation
+# fix the truth table order after doing the permutation. @see swap
+# @param relationNumber
+# @param rank rank of the given relation
+# @param permutationSemantics specifies how the permutation should be applied. can be either RelationCore.SOURCE or RelationCore.TARGET
+# for example: <p>
+#    for the relation: R(v2,v1,v0) <p>
+#    and the permutation {1,2,0} 
+#    SOURCE semantics means that v0 goes to position1, v1 goes to position2, v2 goes to position 0
+#    TARGET semantics means that position0 gets v1, position1 gets v2, position2 gets v0
+# @param permutation an array of variable positions describing the desired location of every variables
+# for example: <p>
+#    for the relation: R(v2,v1,v0) <p>
+#    and the permutation {1,2,0} means v0 goes to position1, v1 goes to position2, v2 goes to position 0
+# @return the modified relationNumber
+def renme(uint32_t rn, int rank, int permutationSemantics, permutation):
+    # checkRank(rank);
+    # checkRelationNumber(rn, rank);
+    # checkPermutation(rank, permutation);
+    # checkPermutationSemantics(permutationSemantics);
+    
+    # sort dimensions
+    cdef int i
+    cdef int j
+    cdef int min
+    cdef int tmp
+
+    for 0 <= i < rank - 1:
+        min = i; #  minimum
+        for i + 1 <= j < rank:
+            #  inner loop
+            if (permutation[j] < permutation[min]): #  if min greater,
+                min = j; #  a new min
+        # swap elements at min, i
+        tmp = permutation[i]
+        permutation[i] = permutation[min]
+        permutation[min] = tmp
+        # see
+
+        if permutationSemantics == SOURCE:
+            rn = swap(rn, rank, permutation[min], permutation[i])
+        elif permutationSemantics == TARGET:
+            rn = swap(rn, rank, min, i)
+        else:
+            pass # error
+    return rn
+
+
+# returns the number of ones in the given relationNumber
+# @param relationNumber
+# @param rank
+# @return
+def ones(uint32_t rn, int rank):
+    # checkRank(rank);
+    # checkRelationNumber(relationNumber, rank);
+
+    cdef int c
+    cdef int i
+
+    c = 0
+    for 0 <= i < (1 << rank):
+        if ((rn & (1 << i)) != 0):
+            c += 1
+    return c
+
+
+
+# @param relationNumber
+# @param rank
+# @param num_true_vars used to identify a set of rows in the truth table
+# @return counts the number of ones corresponding to truth table rows with the given number of true variables
+def q(uint32_t rn, int rank, int num_true_vars):
+    # checkRank(rank);
+    # checkRelationNumber(relationNumber, rank);
+
+    cdef int m
+    if num_true_vars > rank:
+        return -1
+
+    m = x_true_vars(rank, num_true_vars);
+    return ones(rn & m, rank)
+
+
+
+# for 1in3 use xTrueVars(3,1)
+# @param rank
+# @param num_true_vars
+# @return an integer representing the relation number which is true only when the given number of vars is true
+def x_true_vars(int rank, int num_true_vars):
+    cdef uint32_t rn
+    cdef int i
+
+    rn = 0
+
+    for 0 <= i < (1 << rank):
+        # RelationCore.ones(i, 3) a truth table row can have up to 5 columns
+        # RelationCore.ones(i, 3) will count ones up to the 8th column
+        if(ones(i, 3) == num_true_vars): 
+            rn |= (1 << i)
+    return rn
