77baed0b6e9f5bbebb001b995bbdf72cef9c5d5b
Will Nowak <compbrain@gmail.com>
2009-09-23 14:52:23 -0400

    Updated gameplay logic

--- src/game.py
+++ src/game.py
@@ -46,12 +46,13 @@ class Config(object):
 
 
 class PlayerContext(object):
-  def __init__(self, config=None, offered=None, provided=None, accepted=None, 
-               solved=None, playerid=None):
-    self.offered = offered
-    self.provided = provided
+  def __init__(self, config=None, their_offered=None,
+               our_offered=None, accepted=None, 
+               provided=None, playerid=None):
+    self.their_offered = their_offered
+    self.our_offered = our_offered
     self.accepted = accepted
-    self.solved = solved
+    self.provided = provided
     self.config = config
     self.playerid = int(playerid)
 
@@ -64,8 +65,9 @@ class PlayerContext(object):
   @classmethod
   def FromParsed(cls, parsed):
     return cls(config=Config.FromParsed(parsed.config),
-               provided=parsed.their_offered, offered=parsed.our_offered,
-               accepted=parsed.accepted, solved=parsed.solved,
+               their_offered=parsed.their_offered,
+               our_offered=parsed.our_offered,
+               accepted=parsed.accepted, provided=parsed.provided,
                playerid=parsed.playerid)
 
 
@@ -84,7 +86,8 @@ def DoRegistration(server, ourport, ourteam, ourpass):
       return '%s registration success!' % ourteam
     else:
       return '"%s" != "%s"' % (str(resp), str(ourteam))
-  except urllib2.HTTPError, e:
+  except Exception, e:
+    logging.exception('Reg Failure at %s ' % regurl)
     return '%s registration FAILURE! (%s)' % (ourteam, str(e))
 
 class Game(object):
@@ -93,6 +96,11 @@ class Game(object):
     self.id = int(self.context.playerid)
 
   def RunTasks(self):
+    self.offers = []
+    logging.debug('Their Offered: %s' % str(self.context.their_offered))
+    logging.debug('Our Offered: %s' % str(self.context.our_offered))
+    logging.debug('Accepted: %s' % str(self.context.accepted))
+    logging.debug('Provided: %s' % str(self.context.provided))
     logging.info('Running all tasks...')
     for x in [self.AcceptTask, self.ProvideTask, self.SolveTask,
               self.OfferTask, self.ReofferTask]:
@@ -100,28 +108,39 @@ class Game(object):
 
   def AcceptTask(self):
     logging.debug('Running AcceptTask')
-    logging.debug('Offered: %s' % str(self.context.offered))
-    logging.debug('accepted: %s' % str(self.context.accepted))
-    logging.debug('provided: %s' % str(self.context.provided))
-    logging.debug('solved: %s' % str(self.context.solved))
+    logging.debug(self.context.their_offered.asDict())
 
   def ProvideTask(self):
     logging.debug('Running ProvideTask')
+    otheroffers = list(self.context.accepted)
+    while otheroffers:
+      id, bla, blah, blaah, price = otheroffers[:5]
+      otheroffers = otheroffers[5:]
+      self.offers.append('provide[v0 v1 v2 v3 v4  (229 v3 v4 v0 ) (229 v0 v1 v2 ) (229 v3 v4 v0 ) (229 v3 v4 v0 ) (229 v2 v3 v4 ) (229 v4 v0 v1 ) (229 v0 v1 v2 ) (229 v3 v4 v0 ) (229 v0 v1 v2 ) (229 v4 v0 v1 ) %s ]' % id)
 
   def SolveTask(self):
     logging.debug('Running SolveTask')
 
   def OfferTask(self):
     logging.debug('Running OfferTask')
+    self.offers.append('offer[(229) 0.8710813713911519]')
 
   def ReofferTask(self):
     logging.debug('Running ReofferTask')
+    otheroffers = list(self.context.their_offered) + list(self.context.our_offered)
+    while otheroffers:
+      id, bla, blah, price = otheroffers[:4]
+      otheroffers = otheroffers[4:]
+      self.offers.append('reoffer[%d %0.8f]' % (int(id), float(price)-0.01))
 
   def GenerateReply(self):
     logging.info('Generating Game Reply')
-    return ('\nplayertrans[\n    %d\n    offer[(3 ) 0.8710813713911519] \n'
-            '    offer[(3 ) 0.8710813713911519] \n'
-            '    accept[ %s ] \n]\n' % (self.id, self.context.provided[0]))
+    r = '\nplayertrans[\n    %d\n' % self.id
+    for o in self.offers:
+      r += '    %s \n' % o
+    r +=']\n'
+    logging.info('Replying with: %s' % r)
+    return r
 
   @classmethod
   def Play(cls, gamedata):
